03da15b399d338e6c48d12249852e5b2a547985a
n0tpurplx <practicalcharacter@gmail.com>
2026-04-10 17:30:28 +0200

    Add initial structure for Spicker.py with commands

---
+++ Spicker.py
@@ -0,0 +1,17 @@
+# minus 0.25 punkte pro verwendung!
+
+# =========================≈==
+# 1. Imports
+# ============================
+import discord
+from discord import app_commands
+# (optional loegt dran was du machst) import asyncio
+
+
+# ============================
+# 2. Commands
+# ========≈===================
+
+@tree.command(name='name', description='beschreibung')
+async def name(interaction: discord.Interaction):
+  # der code kommt dann hier
